200e76da6df0a115f05c86ed897fd53e24b85d86
Manuele Menozzi <manuele.menozzi@gmail.com>
2021-04-23 17:33:48 +0200

    Rename Acme\SyliusExamplePlugin to Webgriffe\SyliusNexiPlugin

--- src/WebgriffeSyliusNexiPlugin.php
+++ src/WebgriffeSyliusNexiPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Webgriffe\SyliusNexiPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class WebgriffeSyliusNexiPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
