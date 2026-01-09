16c9cf77020a3c1ce47e0636b0d36f96074d72c4
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-01-09 12:26:17 +0100

    [HOTFIX] Remove wrong route definition

--- config/shop_ajax_routing.php
+++ config/shop_ajax_routing.php
@@ -5,9 +5,4 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->add('webgriffe_sylius_klarna_payments_plugin_payment_status', '/payment/{paymentId}/klarna-payments-status')
-        ->controller(['webgriffe_sylius_klarna_payments.controller.payment', 'statusAction'])
-        ->methods(['GET'])
-        ->requirements(['paymentId' => '\d+'])
-    ;
 };
